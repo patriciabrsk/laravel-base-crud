5a7c36d9b7981d7ace09677e132706dde7f66579
patriciabrsk <patriciabresku@gmail.com>
2022-05-17 22:18:20 +0200

    create blade

--- resources/views/comics/create.blade.php
+++ resources/views/comics/create.blade.php
@@ -6,27 +6,46 @@
     <div class="container py-5">
         <div class="row">
             <div class="row text-center">
-                <div class="col-12 d-flex align-items-center justify-content-center">
-                    <div class="card">
-                    <div class="card-body">
-                            <img src="{{ $comic['thumb'] }}" alt="">
-                            <h5 class="card-title">
-                                {{ $comic->title }}
-                                <a href="{{ route('comics.show', $comic->id) }}">
-                                    {{ $comic->id }}
-                                </a>
-                            </h5>
-                            <p class="card-text">
-                                {{ $comic-> description}} 
-                            </p>
-                            <p class="card-text">
-                                {{ $comic->price }} <span>- {{ $comic->series }}</span>
-                            </p>
-                            <p class="card-text">
-                                {{ $comic->sale_date }} <span>- {{ $comic->type }}</span>
-                            </p>
-                        </div>
+                <div class="col-6 d-flex align-items-center justify-content-center">
+                    
+                <form action="{{ route('comics.store') }}" method="post">
+                    <div class="form-group">
+                        <label for="title">Title</label>
+                        <input type="text" class="form-control" id="title" placeholder="Insert title">
                     </div>
+
+                    <div class="form-group">
+                        <label for="description">Description</label>
+                        <textarea class="form-control" id="descritpion" placeholder="Insert descsription" rows="3"></textarea>
+                    </div>
+
+                    <div class="form-group">
+                        <label for="thumb">Thumb</label>
+                        <input type="text" class="form-control" id="thumb" placeholder="Insert img thumb">
+                    </div>
+
+                    <div class="form-group">
+                        <label for="price">Price</label>
+                        <input type="number" class="form-control" id="price" placeholder="Insert price">
+                    </div>
+
+                    <div class="form-group">
+                        <label for="series">Series</label>
+                        <input type="text" class="form-control" id="series" placeholder="Insert series" name="series">
+                    </div>
+
+                    <div class="form-group">
+                        <label for="sale-date">Sale date</label>
+                        <input type="date" class="form-control" id="sale-date" placeholder="Insert sale date" name="sale_date">
+                    </div>
+
+                    <div class="form-group">
+                        <label for="type">Comic type</label>
+                        <input type="text" class="form-control" id="type" placeholder="Insert type" name="type">
+                    </div>
+
+                    <button class="btn btn-primary" type="submit">Submit</button>
+                </form>
                 </div>
             </div>
         </div>
