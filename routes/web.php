ac8cecfe352376e9fd0f2f154dae0d8a627981a0
patriciabrsk <patriciabresku@gmail.com>
2022-05-17 19:29:18 +0200

    update web.php

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'Comicontroller@index')->name('comics.index');
+Route::get('/comic/{id}', 'ComicController@show')->name('comics.show');
